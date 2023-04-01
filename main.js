bd3cd8b5b47c09634ad3782b84f33d52dc318525
kevinj0695 <78401891+kevinj0695@users.noreply.github.com>
2023-04-01 16:41:56 +0900

    모달, 오디오 기능 추가 (게임 마무리)
    modal - win, lose, retry 종류를 만들어 각각의 상황에 맞는 모달을 띄움
    audio - 상황의 맞는 오디오 출력
    
    게임 마무리 완료!

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const gameFields = document.querySelector('.game__field');
 
+// deal with coordinates
 function coordAssign() {
   const carrots = document.querySelectorAll('.carrot');
   const bugs = document.querySelectorAll('.bug');
@@ -12,44 +13,56 @@ function coordAssign() {
     bug.style.left = `${getCoordX()}px`;
   })
 }
-
 function getCoordX() {
   const x = gameFields.getBoundingClientRect().x;
   const right = gameFields.getBoundingClientRect().right;
   return Math.random() * (right - x);
 }
-
 function getCoordY() {
   const y = gameFields.getBoundingClientRect().y;
   const bottom = gameFields.getBoundingClientRect().bottom;
   return Math.random() * (bottom - y);
 }
 
+// deal with Btns
 const startBtn = document.querySelector('.game__start-btn');
 const stopBtn = document.querySelector('.game__stop-btn');
+const retryBtn = document.querySelector('.game__retry-btn');
 startBtn.addEventListener('click', () => {
   gameFields.classList.add('show');
   coordAssign();
   setTimer(10, 0);
   startBtn.classList.remove('show');
   stopBtn.classList.add('show');
+  startBgAudio();
 });
-stopBtn.addEventListener('click', () => {
+const stopBtnListener = () => {
   stopTimer();
-})
+  showModal('retry');
+}
+stopBtn.addEventListener('click', stopBtnListener);
+retryBtn.addEventListener('click', () => {
+  document.location.reload();
+});
 
-gameFields.addEventListener('click', (event) => {
+// Game Fields Listener
+const gameFieldsListener = (event) => {
   const target = event.target;
-  if (target.classList[0] !== 'game__field') {
-    if (target.classList[0] === 'carrot') {
-      carrotCounter();
-    }
+  if (target.classList[0] === 'carrot') {
+    carrotCounter();
+    target.remove();
+  } else if (target.classList[0] === 'bug') {
+    pullBugAudio();
+    stopTimer();
+    showModal('lose');
     target.remove();
   }
-});
+}
+gameFields.addEventListener('click', gameFieldsListener);
 
+
+// Timer
 let timerId;
-
 function setTimer(from, to) {
   let current = from;
   const timer = document.querySelector('.game__timer');
@@ -57,20 +70,90 @@ function setTimer(from, to) {
     timer.innerText = `0:0${--current}`;
     if (current === to) {
       clearInterval(timerId);
+      showModal('lose');
     }
   }, 1000);
 }
-
-function carrotCounter() {
-  const gameCounter = document.querySelector('.game__counter');
-  gameCounter.innerText = gameCounter.innerText - 1;
-  
-  if (gameCounter.innerText == 0) {
-    stopTimer();
-  }
-}
-
 function stopTimer() {
   clearInterval(timerId);
   timerId = null;
+}
+
+// counter
+function carrotCounter() {
+  const gameCounter = document.querySelector('.game__counter');
+  gameCounter.innerText = gameCounter.innerText - 1;
+  pullCarrotAudio();
+
+  if (gameCounter.innerText == 0) {
+    stopTimer();
+    showModal('win');
+  }
+}
+
+// modals
+const modal = document.querySelector('.game__modals');
+const modalWin = document.querySelector('.modal--win');
+const modalLose = document.querySelector('.modal--lose');
+const modalRetry = document.querySelector('.modal--retry');
+
+function showModal(property) {
+  switch (property) {
+    case 'win' :
+      modal.classList.add('show');
+      modalWin.classList.add('show');
+      winGameAudio();
+      break;
+    case 'lose' :
+      modal.classList.add('show');
+      modalLose.classList.add('show');
+      alertGameAudio();
+      break;
+    case 'retry' :
+      modal.classList.add('show');
+      modalRetry.classList.add('show');
+      alertGameAudio();
+      break;
+    default:
+      console.error('property에 case가 아닌 값이 들어옴');
+  }
+
+  stopBtn.removeEventListener('click', stopBtnListener);
+  gameFields.removeEventListener('click', gameFieldsListener);
+}
+
+// audio
+const bgAudio = document.querySelector('.bg-audio');
+function startBgAudio() {
+  bgAudio.volume = 0.5;
+  bgAudio.play();
+}
+function pauseBgAudio() {
+  bgAudio.pause();
+}
+
+const carrotAudio = document.querySelector('.carrot-audio');
+function pullCarrotAudio() {
+  carrotAudio.volume = 0.5;
+  carrotAudio.play();
+}
+
+const bugAudio = document.querySelector('.bug-audio');
+function pullBugAudio() {
+  bugAudio.volume = 0.5;
+  bugAudio.play();
+}
+
+const winAudio = document.querySelector('.win-audio');
+function winGameAudio() {
+  pauseBgAudio();
+  winAudio.volume = 0.5;
+  winAudio.play();
+}
+
+const alertAudio = document.querySelector('.alert-audio');
+function alertGameAudio() {
+  pauseBgAudio();
+  alertAudio.volume = 0.5;
+  alertAudio.play();
 }
