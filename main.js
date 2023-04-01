07ebcbd72bddb64d8a53f5cb406752856f1394d4
kevinj0695 <78401891+kevinj0695@users.noreply.github.com>
2023-04-01 14:21:58 +0900

    카운터기능, 타이머스톱 기능 추가

--- main.js
+++ main.js
@@ -34,21 +34,43 @@ startBtn.addEventListener('click', () => {
   startBtn.classList.remove('show');
   stopBtn.classList.add('show');
 });
+stopBtn.addEventListener('click', () => {
+  stopTimer();
+})
 
 gameFields.addEventListener('click', (event) => {
   const target = event.target;
   if (target.classList[0] !== 'game__field') {
+    if (target.classList[0] === 'carrot') {
+      carrotCounter();
+    }
     target.remove();
   }
 });
 
+let timerId;
+
 function setTimer(from, to) {
   let current = from;
   const timer = document.querySelector('.game__timer');
-  let timerId = setInterval(() => {
+  timerId = setInterval(() => {
     timer.innerText = `0:0${--current}`;
     if (current === to) {
       clearInterval(timerId);
     }
-  }, 1000)
+  }, 1000);
+}
+
+function carrotCounter() {
+  const gameCounter = document.querySelector('.game__counter');
+  gameCounter.innerText = gameCounter.innerText - 1;
+  
+  if (gameCounter.innerText == 0) {
+    stopTimer();
+  }
+}
+
+function stopTimer() {
+  clearInterval(timerId);
+  timerId = null;
 }
